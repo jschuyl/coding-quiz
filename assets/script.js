d9a334e71014a52a69c1dfb4902fc8b9a3af646d
jschuyl <schuylertjed@gmail.com>
2022-08-21 13:07:37 -0700

    added question variables, started random question choice

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+
+// All 10 questions down below
+var Q1 = {
+    question: "Of the options below, what is a primitive type?",
+    choices: ['A', 'B', 'C', 'D'],
+    answer: 'C',
+};
+var Q2 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q3 ={
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q4 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q5 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q6 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q7 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q8 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q9 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+var Q10 = {
+    question: '',
+    choices: ['A', 'B', 'C', 'D'],
+    answer: '',
+};
+// where I am drawing my question options from
+var Qarray = [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10];
+
+function whatQuestion() {
+    for (let i = 0; i < Qarray.length; i++);
+    
+}
